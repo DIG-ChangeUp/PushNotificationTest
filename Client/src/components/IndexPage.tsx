c233fecad5dd1319a85d1015ddc2bd72ee3a0257
QaMuu <44154881+QaMuu@users.noreply.github.com>
2024-12-16 14:25:38 +0900

    通知用素材アップロード

--- Client/src/components/IndexPage.tsx
+++ Client/src/components/IndexPage.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+import OneSignal from 'react-onesignal'
+
 export function IndexPage() {
+	useEffect(() => {
+		(async() => {
+			OneSignal.init({
+				appId: 'c890d157-b1e0-4779-8222-465dd83359ff',
+			})
+		})()
+	}, [])
 	return(
 		<>
 			<h1>Index Page</h1>
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Test App',
+        short_name: 'Test',            // ホーム画面に追加した時に表示される名前
+        description: '通知テストアプリ',
+        theme_color: '#ffffff',
+        // 各サイズごとのアイコンは下記で生成する。
+        // https://www.pwabuilder.com/imageGenerator
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
